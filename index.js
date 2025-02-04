535d233c11d6a7ce177c56861df3f84e281f23bf
Michael McCormack <mcctwin@gmail.com>
2025-02-03 21:36:17 -0600

    9 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    console.log(cats.push("Ralph"))
+}
+function destructivelyPrependCat(name) {
+    console.log(cats.unshift("Bob"))
+}
+function destructivelyRemoveLastCat(name) {
+    console.log(cats.pop())
+}
+function destructivelyRemoveFirstCat(name) {
+    console.log(cats.shift())
+}
+
+function appendCat(name) {
+    return([...cats, name])
+}
+function prependCat(name) {
+    return([name, ...cats])
+}
+function removeLastCat(name) {
+    return(cats.slice(0, 2))
+}
+function removeFirstCat(name) {
+    return(cats.slice( 1, 3))
+}
